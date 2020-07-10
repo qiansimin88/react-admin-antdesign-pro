c2812ceb1b812333610538379fc12e17a7c5e98b
qsm <qiansimin@xinc818.group>
2020-07-10 16:23:20 +0800

    fix

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,37 +1,10 @@
 import React, { memo, useEffect, useState, useCallback, useRef } from 'react';
-import { Input } from 'antd';
-import useEffectUpdate from '@/components/hook/useEffectUpdate'
 
 // 供应商订单
 const Test = () => {
-
-  // const [state, setState]  = useState(1)
-
-  const demoRef = useRef(0)
-
-  const dd = useCallback(() => {
-    demoRef.current  =  demoRef.current + 1
-  }, [])
-// 
-  // useEffect(() => {
-    // console.log(  demoRef.current )
-  // })
-
-  const haha = useCallback(() => {
-    console.log(  demoRef.current )
-  })
-
-  // useEffectUpdate(() => {
-    // console.log(33333)
-  // }, [state])
-
-
     return (
       <>
-          {/* <Input value={ value } placeholder="Basic usage" onChange={ onChange }/> */}
-          <div>now: { demoRef.current }</div>
-          <div onClick={() => dd()}>112223</div>
-          <div onClick={ () => haha() }>22222</div>
+        test page for xcli
       </>
     )
 }
